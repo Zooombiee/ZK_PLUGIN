f735e82376aac2feed8e496f3b18b80adda147ef
GitHub Action <action@github.com>
2022-02-28 08:48:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/zooombiee/zkplugin/MyBundle.kt
+++ src/main/kotlin/com/github/zooombiee/zkplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zooombiee.zkplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zooombiee/zkplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zooombiee/zkplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zooombiee.zkplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zooombiee.zkplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
